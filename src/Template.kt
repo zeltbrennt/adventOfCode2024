2206bc434e37c6fe021fbe100ed20617a729cc79
zeltbrennt <zeltbrennt@gmail.com>
2024-12-02 07:45:09 +0100

    Update Template

--- src/Template.kt
+++ src/Template.kt
@@ -1,6 +1,8 @@
 import kotlin.time.measureTime
 
 fun main() {
+
+
     fun part1(input: List<String>): Int {
         return 0
     }
@@ -9,10 +11,11 @@ fun main() {
         return 0
     }
 
+    // test before attempt to solve
     val testInput = readInput("Day00_test")
     check(part1(testInput) == 0)
 
-    // Read the input from the `src/Day00.txt` file.
+    // solve with real input
     val input = readInput("Day00")
     measureTime { print("Part 1: ${part1(input)}".padEnd(40, ' ')) }.also { println("$it") }
     measureTime { print("Part 2: ${part2(input)}".padEnd(40, ' ')) }.also { println("$it") }
