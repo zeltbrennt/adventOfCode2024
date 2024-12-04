d7ee810c9e0fbfef5edf0590885f8c75c7525618
zeltbrennt <zeltbrennt@gmail.com>
2024-12-04 08:45:44 +0100

    Implement Grid

---
+++ src/Grid.kt
@@ -0,0 +1,68 @@
+data class Coord(val x: Int, val y: Int) {
+
+    /*
+     o --> +1
+     |
+     v
+     +1
+     */
+    fun north(): Coord {
+        return this.copy(y = y - 1)
+    }
+
+    fun south(): Coord {
+        return this.copy(y = y + 1)
+    }
+
+    fun east(): Coord {
+        return this.copy(x = x + 1)
+    }
+
+    fun west(): Coord {
+        return this.copy(x = x - 1)
+    }
+
+    fun northWest(): Coord {
+        return this.copy(x = x - 1, y = y - 1)
+    }
+
+    fun northEast(): Coord {
+        return this.copy(x = x + 1, y = y - 1)
+    }
+
+    fun southWest(): Coord {
+        return this.copy(x = x - 1, y = y + 1)
+    }
+
+    fun southEast(): Coord {
+        return this.copy(x = x + 1, y = y + 1)
+    }
+
+    fun neighbor(direction: Direction): Coord {
+        return when (direction) {
+            Direction.NORTH -> north()
+            Direction.SOUTH -> south()
+            Direction.EAST -> east()
+            Direction.WEST -> west()
+            Direction.NORTHEAST -> northEast()
+            Direction.SOUTHEAST -> southEast()
+            Direction.NORTHWEST -> northWest()
+            Direction.SOUTHWEST -> southWest()
+        }
+    }
+}
+
+typealias Grid<T> = Map<Coord, T>
+
+enum class Direction { NORTH, SOUTH, EAST, WEST, NORTHEAST, SOUTHEAST, NORTHWEST, SOUTHWEST }
+
+val allDirections = listOf(
+    Direction.NORTH,
+    Direction.SOUTH,
+    Direction.WEST,
+    Direction.EAST,
+    Direction.NORTHWEST,
+    Direction.NORTHEAST,
+    Direction.SOUTHWEST,
+    Direction.SOUTHEAST,
+)
