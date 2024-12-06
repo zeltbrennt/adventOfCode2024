e11cae43a77bc0adb86801b7a13ae6f9fc132cdd
zeltbrennt <zeltbrennt@gmail.com>
2024-12-06 07:54:50 +0100

    Solve Day 06

---
+++ src/Day06.kt
@@ -0,0 +1,81 @@
+fun main() {
+
+    fun inputToGrid(input: List<String>): Pair<Grid<Char>, Coord> {
+        val data = mutableMapOf<Coord, Char>()
+        var start = Coord(0, 0)
+        input.forEachIndexed { y, row ->
+            row.forEachIndexed { x, value ->
+                data[Coord(x, y)] = value
+                if (value == '^') {
+                    start = Coord(x, y)
+                }
+            }
+        }
+        return Grid(data) to start
+    }
+
+    fun Direction.turn90DegreesRight(): Direction {
+        return when (this) {
+            Direction.NORTH -> Direction.EAST
+            Direction.EAST -> Direction.SOUTH
+            Direction.SOUTH -> Direction.WEST
+            Direction.WEST -> Direction.NORTH
+            else -> throw Error("this should not happen")
+        }
+    }
+
+    fun tracePath(grid: Grid<Char>, start: Coord): Set<Coord> {
+        var guard = start
+        var direction = Direction.NORTH
+        val path = mutableSetOf<Coord>()
+        while (grid[guard] != null) {
+            if (grid[Grid.neighbor(guard, direction)] == '#') {
+                direction = direction.turn90DegreesRight()
+            } else {
+                path.add(guard)
+                guard = Grid.neighbor(guard, direction)
+            }
+        }
+        return path
+    }
+
+    fun part1(input: List<String>): Long {
+        val (grid, start) = inputToGrid(input)
+        return tracePath(grid, start).size.toLong()
+    }
+
+    fun isLoop(grid: Grid<Char>, start: Coord, startDirection: Direction, obstacle: Coord): Boolean {
+        var guard = start
+        var direction = startDirection
+        val path = mutableMapOf<Coord, Direction>()
+        while (grid[guard] != null) {
+            if (path[guard] == direction) {
+                return true
+            }
+            if (grid[Grid.neighbor(guard, direction)] == '#' || Grid.neighbor(guard, direction) == obstacle) {
+                direction = direction.turn90DegreesRight()
+            } else {
+                path[guard] = direction
+                guard = Grid.neighbor(guard, direction)
+            }
+        }
+        return false
+    }
+
+    fun part2(input: List<String>): Long {
+        val (grid, start) = inputToGrid(input)
+        val path = tracePath(grid, start)
+
+        return path.count { obstacle ->
+            isLoop(grid, start, Direction.NORTH, obstacle)
+        }.toLong()
+    }
+
+    // test before attempt to solve
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 41L)
+    check(part2(testInput) == 6L)
+
+    // solve with real input
+    solve("Day06", ::part1, ::part2)
+}
