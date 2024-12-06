37b682919c0cbc97511874bcede80d35695041f4
zeltbrennt <zeltbrennt@gmail.com>
2024-12-06 08:01:46 +0100

    Adjust Template

--- src/Template.kt
+++ src/Template.kt
@@ -1,17 +1,18 @@
 fun main() {
 
 
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Number {
         return 0
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Number {
         return 0
     }
 
     // test before attempt to solve
     val testInput = readInput("Day00_test")
     check(part1(testInput) == 0L)
+    check(part2(testInput) == 0L)
 
     // solve with real input
     solve("Day00", ::part1, ::part2)
