27ece48c46911c2d4ea8490e037abf88f637adc8
zeltbrennt <zeltbrennt@gmail.com>
2024-12-08 07:45:52 +0100

    Solve Day 08

--- src/Day08.kt
+++ src/Day08.kt
@@ -25,13 +25,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Number {
-        return 0
+        val cityMap: Grid<Antenna> = inputToSparseGrid(input)
+        val antiFrequencies = mutableSetOf<Coord>()
+        val visited = mutableSetOf<Antenna>()
+        for (antenna in cityMap.values) {
+            if (antenna in visited) continue
+            visited.add(antenna)
+            val coords = cityMap.data.filter { it.value == antenna }.keys
+            for (a in coords) {
+                for (b in coords) {
+                    if (a == b) continue
+                    val relative = a - b
+                    var i = 0
+                    while (true) {
+                        val antiFreq = a + relative * i
+                        if (antiFreq.x in input[0].indices && antiFreq.y in input.indices) {
+                            antiFrequencies.add(antiFreq)
+                            i++
+                        } else {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        return antiFrequencies.count()
     }
 
     // test before attempt to solve
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 14)
-    //check(part2(testInput) == 0L)
+    check(part2(testInput) == 34)
 
     // solve with real input
     solve("Day08", ::part1, ::part2)
