1578ef7eb3a6d89de7202199c94556b4675c22eb
zeltbrennt <zeltbrennt@gmail.com>
2024-12-10 11:54:47 +0100

    use Grid

--- src/Day10.kt
+++ src/Day10.kt
@@ -9,7 +9,7 @@ fun main() {
         }
     }
 
-    fun Coord.score(topMap: Map<Coord, Int>, peaksRached: MutableSet<Coord>, current: Int = 0): MutableSet<Coord> {
+    fun Coord.score(topMap: Grid<Int>, peaksRached: MutableList<Coord>, current: Int = 0): MutableList<Coord> {
         if (current == 9) {
             peaksRached.add(this)
         }
@@ -32,38 +32,14 @@ fun main() {
         return peaksRached
     }
 
-    fun Coord.rating(topMap: Map<Coord, Int>, current: Int = 0): Int {
-        if (current == 9) {
-            return 1
-        }
-        var rating = 0
-        val west = Grid.west(this)
-        val east = Grid.east(this)
-        val north = Grid.north(this)
-        val south = Grid.south(this)
-        if ((topMap[west] ?: 0) == current + 1) {
-            rating += west.rating(topMap, current + 1)
-        }
-        if ((topMap[east] ?: 0) == current + 1) {
-            rating += east.rating(topMap, current + 1)
-        }
-        if ((topMap[north] ?: 0) == current + 1) {
-            rating += north.rating(topMap, current + 1)
-        }
-        if ((topMap[south] ?: 0) == current + 1) {
-            rating += south.rating(topMap, current + 1)
-        }
-        return rating
-    }
-
     fun part1(input: List<String>): Number {
-        val topoMap = parse(input)
-        return topoMap.filter { it.value == 0 }.keys.sumOf { it.score(topoMap, mutableSetOf()).size }
+        val topoMap = Grid(parse(input))
+        return topoMap.data.filter { it.value == 0 }.keys.sumOf { it.score(topoMap, mutableListOf()).toSet().size }
     }
 
     fun part2(input: List<String>): Number {
-        val topoMap = parse(input)
-        return topoMap.filter { it.value == 0 }.keys.sumOf { it.rating(topoMap) }
+        val topoMap = Grid(parse(input))
+        return topoMap.data.filter { it.value == 0 }.keys.sumOf { it.score(topoMap, mutableListOf()).size }
     }
 
     // test before attempt to solve
