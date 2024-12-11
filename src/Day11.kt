656805d7c8b9a80bd8e8028240a1dcdc53b1b343
zeltbrennt <zeltbrennt@gmail.com>
2024-12-11 11:00:34 +0100

    Solve Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -2,48 +2,48 @@ import kotlin.math.pow
 
 fun main() {
 
-    data class Counter(var value: Long)
-
-    fun blink(stone: Long, times: Int, counter: Counter) {
+    fun blink(stone: Long, depth: Int, memo: MutableMap<Pair<Long, Int>, Long>) : Long{
         val nDigits = getNumberOfDigits(stone)
         when {
-            times == 0 -> {
-                counter.value++
-               // print("$stone ")
+            stone to depth in memo -> return memo[stone to depth]!!
+            depth == 0 -> {
+                memo[stone to 0] = 1
+                return 1
             }
-            stone == 0L -> blink(1, times -1, counter)
+            stone == 0L -> {
+                memo[0L to depth] = blink(1, depth - 1, memo)
+                return memo[0L to depth]!!
+            }
             nDigits % 2 == 0 -> {
                 val split = 10.0.pow(nDigits / 2).toLong()
-                blink(stone / split, times - 1,counter)
-                blink(stone % split, times - 1,counter)
+                memo[stone to depth] =  blink(stone / split, depth - 1,memo) +
+                                        blink(stone % split, depth - 1,memo)
+                return memo[stone to depth]!!
             }
-            else -> blink(stone * 2024, times -1, counter)
+            else -> {
+                memo[stone to depth] = blink(stone * 2024, depth - 1, memo)
+                return memo[stone to depth]!!
+            }
         }
 
     }
 
     fun part1(input: List<String>): Number {
-        val counter = Counter(0)
-        input[0].split(" ").map { it.toLong() }.forEach {
-            blink(it, 25, counter)
-        }
-       // counter.println()
-        return counter.value
+         return input[0].split(" ").sumOf {
+             blink(it.toLong(), 25, mutableMapOf())
+         }
     }
 
     fun part2(input: List<String>): Number {
-        val counter = Counter(0)
-        input[0].split(" ").map { it.toLong()}.forEach {
-            blink(it, 6, counter)
+        return input[0].split(" ").sumOf {
+            blink(it.toLong(), 75, mutableMapOf())
         }
-        //counter.println()
-        return counter.value
     }
 
     // test before attempt to solve
     val testInput = readInput("Day11_test")
     check(part1(testInput) == 55312L)
-    check(part2(testInput) == 22L)
+    //check(part2(testInput) == 22L)
 
     // solve with real input
     solve("Day11", ::part1, ::part2)
