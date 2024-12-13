6121eba8720d535ae3ec915bca3d9386620fed2c
zeltbrennt <zeltbrennt@gmail.com>
2024-12-13 10:52:29 +0100

    Solve Day 13

--- src/Day13.kt
+++ src/Day13.kt
@@ -3,76 +3,67 @@ import java.util.PriorityQueue
 
 fun main() {
 
-    data class Button(val displacement: Coord, val cost: Int)
-    data class Machine(val buttonA: Button, val buttonB: Button, val prize: Coord)
-    data class Node(val position: Coord, val a: Int, val b: Int, val cost: Int)
-
-    fun lineToCoord(line: String): Coord {
-        val nums = Regex("\\d+").findAll(line).map { it.value.toInt() }
-        return Coord(nums.first(), nums.last())
+    data class Equation(val a: Long, val b: Long, val p: Long) {
+        operator fun plus(other: Equation) = this.copy(a = this.a + other.a, b = this.b + other.b, p = this.p + other.p)
     }
 
-    fun parse(input: List<String>): List<Machine> = buildList {
+    fun lineToEquation(line: String): Pair<Long, Long> {
+        val (a, b) = Regex("\\d+").findAll(line).toList().map { it.value.toLong() }
+        return a to b
+    }
+
+    fun parse(input: List<String>, offset: Long): List<Pair<Equation, Equation>> = buildList {
         for (i in 0..input.lastIndex step 4) {
-            this.add(
-                Machine(
-                    Button(lineToCoord(input[i]), 3),
-                    Button(lineToCoord(input[i + 1]), 1),
-                    lineToCoord(input[i + 2])
-                )
+            val a = lineToEquation(input[i])
+            val b = lineToEquation(input[i + 1])
+            val x = lineToEquation(input[i + 2])
+            val e1 = Equation(
+                a.first,
+                b.first,
+                (x.first + offset)
             )
+            val e2 = Equation(
+                a.second,
+                b.second,
+                (x.second + offset)
+            )
+            this.add(e1 to e2)
         }
     }
 
-    fun findMinimumTokenToWin(machine: Machine): Node {
-        val queue = PriorityQueue<Node>(compareBy { it.cost })
-        val visited = mutableSetOf<Node>()
-        queue.add(Node(machine.buttonA.displacement, 1, 0, machine.buttonA.cost))
-        queue.add(Node(machine.buttonB.displacement, 0, 1, machine.buttonB.cost))
-        visited.addAll(queue)
-        while (queue.isNotEmpty()) {
-            val current = queue.remove() ?: break
-
-            //println("${queue.size} ${current}")
-            if (current.a > 100 || current.b > 100) continue
-            if (current.position.x > machine.prize.x) continue
-            if (current.position.y > machine.prize.y) continue
-            if (current.position == machine.prize) return current
-            val pressA = Node(
-                current.position + machine.buttonA.displacement,
-                current.a + 1,
-                current.b,
-                current.cost + machine.buttonA.cost
-            )
-            if (!visited.contains(pressA)) {
-                visited.add(pressA)
-                queue.add(pressA)
-            }
-            val pressB = Node(
-                current.position + machine.buttonB.displacement,
-                current.a,
-                current.b + 1,
-                current.cost + machine.buttonB.cost
-            )
-            if (!visited.contains(pressB)) {
-                visited.add(pressB)
-                queue.add(pressB)
-            }
-        }
-        return Node(Coord(0, 0), 0, 0, 0)
+    fun findMinimumTokenToWin(original: Pair<Equation, Equation>): Pair<Long, Long> {
+        // multiply
+        val f1 = original.first.b
+        val f2 = -original.second.b
+        val first =
+            original.first.copy(a = original.first.a * f2, b = original.first.b * f2, p = original.first.p * f2)
+        val second =
+            original.second.copy(a = original.second.a * f1, b = original.second.b * f1, p = original.second.p * f1)
+        // add together
+        val added = first + second
+        // sove for a
+        if (added.p % added.a != 0L) return 0L to 0L
+        val a = added.p / added.a
+        // solve for b
+        val b = (original.first.p - original.first.a * a) / original.first.b
+        // check
+        if (original.second.p == original.second.a * a + original.second.b * b) return a to b
+        return 0L to 0L
     }
 
     fun part1(input: List<String>): Number {
-        return parse(input).sumOf { findMinimumTokenToWin(it).cost }
+        return parse(input, 0).map { findMinimumTokenToWin(it) }.filter { it.first <= 100 && it.second <= 100 }
+            .sumOf { it.first * 3 + it.second }
     }
 
     fun part2(input: List<String>): Number {
-        return 0
+        return parse(input, 10000000000000L).map { findMinimumTokenToWin(it) }
+            .sumOf { it.first * 3 + it.second }
     }
 
     // test before attempt to solve
     val testInput = readInput("Day13_test")
-    check(part1(testInput) == 480)
+    check(part1(testInput) == 480L)
     //check(part2(testInput) == 0L)
 
     // solve with real input
