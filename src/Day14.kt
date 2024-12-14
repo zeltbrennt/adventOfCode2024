6af75c10cb030404cb34d0158750490760fd6c9e
zeltbrennt <zeltbrennt@gmail.com>
2024-12-14 16:48:54 +0100

    Solve Day 14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,3 +1,10 @@
+import java.awt.Dimension
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+import kotlin.math.pow
+import kotlin.math.abs
+
 fun main() {
 
     data class Vector(val position: Coord, val velocity: Coord)
@@ -31,28 +38,50 @@ fun main() {
     }
 
     fun part2(input: List<String>): Number {
-        /*       for (i in 0..10) {
-                   val moved = parse(input).map { simulate(it, i, 101, 103) }
-                   println("$i${"=".repeat(100)}")
-                   for (row in 0..<103) {
-                       buildString {
-                           for (col in 0..<101) {
-                               val c = moved.count { it.x == col && it.y == row }
-                               if (c > 0) this.append(c) else this.append(".")
-                           }
-                       }.println()
-                   }
-               }
-               */
+        val size = Dimension(101, 103)
+
+        val allVX = mutableListOf<Double>()
+        val allVY = mutableListOf<Double>()
+
+        for (i in 0..<10000) { // just a guess...
+
+            val moved = parse(input).map { simulate(it, i, size.width, size.height) }
+
+            val mx = moved.sumOf { it.x } / moved.count().toDouble()
+            val my = moved.sumOf { it.y } / moved.count().toDouble()
+            val vx = moved.sumOf { (it.x - mx).pow(2) }
+            val vy = moved.sumOf { (it.y - my).pow(2) }
+
+            val relx = vx / (allVX.sum() / allVX.count())
+            val rely = vy / (allVY.sum() / allVY.count())
+            allVX.add(vx)
+            allVY.add(vy)
+            if (abs(1 - relx) > 0.2 && abs(1 - rely) > 0.2) { // also guessing...
+
+                val title = i.toString().padStart(4, '0')
+                val img = BufferedImage(size.width, size.height, BufferedImage.TYPE_BYTE_GRAY)
+
+                for (x in 0..<size.width) {
+                    for (y in 0..<size.height) {
+                        val c = moved.count { it.x == x && it.y == y }
+                        if (c > 0) img.setRGB(x, y, 0x0) else img.setRGB(x, y, 0xFFFFFF)
+                    }
+                }
+                ImageIO.write(img, "BMP", File("src/day14_img$title.bmp"))
+
+                return i
+            }
+        }
         return 0
     }
+
     /* test single robot
         for (i in 0..5) {
             simulate(Vector(Coord(2, 4), Coord(2, -3)), i, 11, 7).println()
         }
         println()
     */
-    // test before attempt to solve
+// test before attempt to solve
     val testInput = readInput("Day14_test")
     val moved = parse(testInput).map { simulate(it, 100, 11, 7) }
     /* check visually
@@ -65,8 +94,8 @@ fun main() {
     }
     */
     check(safetyFactor(moved, 11, 7) == 12)
-    //check(part2(testInput) == 0L)
+//check(part2(testInput) == 0L)
 
-    // solve with real input
+// solve with real input
     solve("Day14", ::part1, ::part2)
 }
