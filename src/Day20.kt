e4084e4d757bffc7b09ad3bde33df4f5a48025e4
zeltbrennt <zeltbrennt@gmail.com>
2024-12-20 09:58:26 +0100

    Solve Day 20

--- src/Day20.kt
+++ src/Day20.kt
@@ -2,12 +2,14 @@ fun main() {
 
     tailrec fun dfs(pos: Coord, steps: Int, maze: Map<Coord, Char>, dist: MutableMap<Coord, Int>) {
         dist[pos] = steps
-        when {
-            maze[Grid.north(pos)] != '#' && Grid.north(pos) !in dist -> dfs(Grid.north(pos), steps + 1, maze, dist)
-            maze[Grid.south(pos)] != '#' && Grid.south(pos) !in dist -> dfs(Grid.south(pos), steps + 1, maze, dist)
-            maze[Grid.west(pos)] != '#' && Grid.west(pos) !in dist -> dfs(Grid.west(pos), steps + 1, maze, dist)
-            maze[Grid.east(pos)] != '#' && Grid.east(pos) !in dist -> dfs(Grid.east(pos), steps + 1, maze, dist)
+        val next = when {
+            maze[Grid.north(pos)] != '#' && Grid.north(pos) !in dist -> Grid.north(pos)
+            maze[Grid.south(pos)] != '#' && Grid.south(pos) !in dist -> Grid.south(pos)
+            maze[Grid.west(pos)] != '#' && Grid.west(pos) !in dist -> Grid.west(pos)
+            maze[Grid.east(pos)] != '#' && Grid.east(pos) !in dist -> Grid.east(pos)
+            else -> return
         }
+        dfs(next, steps + 1, maze, dist)
     }
 
     fun parse(input: List<String>): Pair<Grid<Int>, Set<Coord>> {
@@ -24,35 +26,46 @@ fun main() {
         return Grid(distMap) to tempMaze.filter { it.value == '#' }.keys
     }
 
+    data class BFSNode(val pos: Coord, val dist: Int)
+
+    fun bfs(pos: Coord, maze: Grid<Int>, maxDepth: Int, cheats: MutableMap<Pair<Coord, Coord>, Int>) {
+        val visited = mutableSetOf(pos)
+        val queue = ArrayDeque<BFSNode>()
+        queue.addLast(BFSNode(pos, 0))
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            if (current.dist > maxDepth) continue
+            if ((maze[current.pos] ?: 0) > maze[pos]!! + current.dist) {
+                cheats[pos to current.pos] = maze[current.pos]!! - maze[pos]!! - current.dist
+            }
+            Grid.mainNeighbors(current.pos).filter { it !in visited }.forEach {
+                visited.add(it)
+                queue.add(BFSNode(it, current.dist + 1))
+            }
+        }
+    }
+
     fun part1(input: List<String>): Number {
         val (maze, walls) = parse(input)
         val cheats = mutableMapOf<Pair<Coord, Coord>, Int>()
 
-        maze.data.forEach { pos, distance ->
-            var shortCutEntry = Grid.north(pos)
-            var shorCutExit = Grid.north(shortCutEntry)
-            if ((maze[shorCutExit] ?: 0) > distance + 2) cheats[shortCutEntry to shorCutExit] =
-                maze[shorCutExit]!! - 2 - distance
-            shortCutEntry = Grid.south(pos)
-            shorCutExit = Grid.south(shortCutEntry)
-            if ((maze[shorCutExit] ?: 0) > distance + 2) cheats[shortCutEntry to shorCutExit] =
-                maze[shorCutExit]!! - 2 - distance
-            shortCutEntry = Grid.east(pos)
-            shorCutExit = Grid.east(shortCutEntry)
-            if ((maze[shorCutExit] ?: 0) > distance + 2) cheats[shortCutEntry to shorCutExit] =
-                maze[shorCutExit]!! - 2 - distance
-            shortCutEntry = Grid.west(pos)
-            shorCutExit = Grid.west(shortCutEntry)
-            if ((maze[shorCutExit] ?: 0) > distance + 2) cheats[shortCutEntry to shorCutExit] =
-                maze[shorCutExit]!! - 2 - distance
+        maze.coords.forEach {
+            bfs(it, maze, 2, cheats)
         }
-
         //cheats.values.groupingBy { it }.eachCount().toSortedMap().forEach(::println)
         return cheats.filter { it.value >= 100 }.count()
     }
 
+
     fun part2(input: List<String>): Number {
-        return 0
+        val (maze, walls) = parse(input)
+        val cheats = mutableMapOf<Pair<Coord, Coord>, Int>()
+
+        maze.coords.forEach {
+            bfs(it, maze, 20, cheats)
+        }
+        //cheats.values.filter { it >= 50 }.groupingBy { it }.eachCount().toSortedMap().forEach(::println)
+        return cheats.filter { it.value >= 100 }.count()
     }
 
     // test before attempt to solve
